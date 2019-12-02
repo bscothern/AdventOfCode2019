282e2a9352c15d9d1b89dcf8f7ff8117bc9e0589
Braden Scothern <BradenScothern@gmail.com>
2019-12-01 20:04:23 -0700

    Added back in Day01 tests

--- Tests/Day01LibTests/Day01LibTests.swift
+++ Tests/Day01LibTests/Day01LibTests.swift
@@ -3,8 +3,16 @@ import XCTest
 
 final class Day01LibTests: XCTestCase {
     func testPart1() {
+        XCTAssertEqual(Module(mass: 12).basicFuelRequiredToMove, 2)
+        XCTAssertEqual(Module(mass: 14).basicFuelRequiredToMove, 2)
+        XCTAssertEqual(Module(mass: 1969).basicFuelRequiredToMove, 654)
+        XCTAssertEqual(Module(mass: 100756).basicFuelRequiredToMove, 33583)
     }
 
     func testPart2() {
+        XCTAssertEqual(Module(mass: 1), Module(mass: 1))
+        XCTAssertEqual(Module(mass: 14).totalFuelRequiredToMove(), 2)
+        XCTAssertEqual(Module(mass: 1969).totalFuelRequiredToMove(), 966)
+        XCTAssertEqual(Module(mass: 100756).totalFuelRequiredToMove(), 50346)
     }
 }
